1ef5b287a035211b4ee403d908efff35d4d860bc
Joshua Geissler <jrgeissler14@gmail.com>
2023-07-03 20:49:20 -0400

    add legal pages

---
+++ app/(legal)/tos/page.tsx
@@ -0,0 +1,55 @@
+export default function Tos() {
+    return (
+      <section className="relative">
+        <div className="max-w-3xl mx-auto px-4 sm:px-6">
+          <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+  
+            {/* Page header */}
+            <div className="text-center pb-12 md:pb-20">
+              <h1 className="text-3xl md:text-4xl font-bold">Terms of Service</h1>
+            </div>
+  
+            {/* Terms of Service content */}
+            <div className="max-w-2xl mx-auto">
+              <h2 className="text-xl font-semibold mb-4">1. Introduction</h2>
+              <p className="text-gray-400 mb-6">
+                Welcome to GeisslerSolutions, provided by JG Cloud Consulting LLC. These Terms of Service ("Terms") govern your use of our website and related services. By accessing or using our website, you agree to be bound by these Terms. If you do not agree with any part of these Terms, you may not use our website.
+              </p>
+  
+              <h2 className="text-xl font-semibold mb-4">2. Account Registration</h2>
+              <p className="text-gray-400 mb-6">
+                In order to access certain features of our website, you may need to register for an account. You must provide accurate and complete information during the registration process. You are responsible for maintaining the confidentiality of your account credentials and for all activities that occur under your account. You agree to notify us immediately of any unauthorized use of your account.
+              </p>
+  
+              <h2 className="text-xl font-semibold mb-4">3. Intellectual Property</h2>
+              <p className="text-gray-400 mb-6">
+                The content on our website, including text, graphics, logos, images, and software, is protected by intellectual property laws. You may not copy, distribute, modify, or create derivative works of the content without our prior written consent.
+              </p>
+  
+              <h2 className="text-xl font-semibold mb-4">4. User Conduct</h2>
+              <p className="text-gray-400 mb-6">
+                When using our website, you agree to abide by the following rules:
+              </p>
+              <ul className="list-disc list-inside text-gray-400 mb-6">
+                <li>Do not engage in any unlawful or fraudulent activities.</li>
+                <li>Do not upload or transmit any harmful or malicious content.</li>
+                <li>Do not interfere with the operation of our website or disrupt the experience of other users.</li>
+              </ul>
+  
+              <h2 className="text-xl font-semibold mb-4">5. Limitation of Liability</h2>
+              <p className="text-gray-400 mb-6">
+                We strive to provide accurate and reliable information on our website. However, we make no warranties or representations about the accuracy or completeness of the content. We shall not be liable for any direct, indirect, incidental, consequential, or exemplary damages resulting from your use of our website.
+              </p>
+  
+              <h2 className="text-xl font-semibold mb-4">6. Governing Law</h2>
+              <p className="text-gray-400 mb-6">
+                These Terms shall be governed by and construed in accordance with the laws of [your jurisdiction]. Any disputes arising out of or relating to these Terms shall be subject to the exclusive jurisdiction of the courts in [your jurisdiction].
+              </p>
+            </div>
+  
+          </div>
+        </div>
+      </section>
+    );
+  }
+  
--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
        </Script>
       <body className={`${inter.variable} ${architects_daughter.variable} font-inter antialiased bg-gray-900 text-gray-200 tracking-tight`}>
         <div className="flex flex-col min-h-screen overflow-hidden">
-          {/* <Header /> */}
+          <Header />
           {children}
         </div>
       </body>

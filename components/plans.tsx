dfa1218308e721267cf123a16674cf93758b26a8
Joshua Geissler <jrgeissler14@gmail.com>
2023-07-24 09:36:45 -0400

    500 price

--- components/plans.tsx
+++ components/plans.tsx
@@ -19,12 +19,12 @@ export default function Plans() {
             {/* monthly */}
             <div className="mx-auto mb-4 w-full">
               <div className="md:pr-4 lg:pr-12 xl:pr-16">
-                <h3 className="h3 mb-3">Monthly</h3>
-                <p className="text-xl text-gray-400 mb-4">No commitment. Pause or cancel anytime.</p>
-                <p className="text-xl text-gray-400">$4,995/m</p>
+                <h3 className="h3 mb-3">Standard</h3>
+                <p className="text-xl text-gray-400 mb-4">One request at a time. Pause or cancel anytime.</p>
+                <p className="text-xl text-gray-400">$495/m</p>
                 <p className="text-sm text-gray-400 mb-6">Pause or cancel anytime</p>
                 <div data-aos="fade-up" data-aos-delay="400">
-                  <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full sm:w-auto sm:mb-0" href="https://buy.stripe.com/dR67uL7zLf8qbIY3ce">Get Started</a>
+                  <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full sm:w-auto sm:mb-0" href="https://buy.stripe.com/9AQ9CT3jvbWe6oE28g">Get Started</a>
                 </div>
                 <div className="text-gray-700 font-medium mt-1 mb-4 pt-5">
                   <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="https://calendly.com/cloudzap/introduction" target="_blank">Book a call</a>
@@ -62,20 +62,20 @@ export default function Plans() {
            {/* yearly */}
             <div className="mx-auto mb-4 w-full">
               <div>
-                <h3 className="h3 mb-3">Yearly</h3>
-                <p className="text-xl text-gray-400 mb-4">Save $1000 per month.</p>
-                <p className="text-xl text-gray-400">$3,995/m</p>
-                <p className="text-sm text-gray-400 mb-6">Billed Annually</p>
+              <h3 className="h3 mb-3">Premium</h3>
+                <p className="text-xl text-gray-400 mb-4">Double the requests. Pause or cancel anytime.</p>
+                <p className="text-xl text-gray-400">$795/m</p>
+                <p className="text-sm text-gray-400 mb-6">Pause or cancel anytime</p>
                 <div data-aos="fade-up" data-aos-delay="400">
-                  <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full sm:w-auto sm:mb-0" href="https://buy.stripe.com/28og1h7zL0dwcN2cMT">Get Started</a>
+                  <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full sm:w-auto sm:mb-0" target="_blank" href="https://buy.stripe.com/bIYcP56vH9O67sIaEN">Get Started</a>
                 </div>
                 <div className="text-gray-700 font-medium mt-1 mb-4 pt-5">
-                  <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="https://calendly.com/cloudzap/introduction" target="_blank">Book a call</a>
+                  <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" target="_blank" href="https://calendly.com/cloudzap/introduction" target="_blank">Book a call</a>
                 </div>
                 <ul className="text-lg text-gray-400 -mb-2 pt-5 border-t border-gray-700">
                   <li className="flex items-center mb-2">
                     <span className="inline-block w-2 h-2 mr-2 bg-gray-400 rounded-full"></span>
-                    <span>One request at a time</span>
+                    <span>Two requests at a time</span>
                   </li>
                   <li className="flex items-center mb-2">
                     <span className="inline-block w-2 h-2 mr-2 bg-gray-400 rounded-full"></span>
@@ -92,6 +92,10 @@ export default function Plans() {
                   <li className="flex items-center mb-2">
                     <span className="inline-block w-2 h-2 mr-2 bg-gray-400 rounded-full"></span>
                     <span>Easy credit-card payments</span>
+                  </li>
+                  <li className="flex items-center mb-2">
+                    <span className="inline-block w-2 h-2 mr-2 bg-gray-400 rounded-full"></span>
+                    <span>Pause or cancel anytime</span>
                   </li>
                 </ul>
               </div>

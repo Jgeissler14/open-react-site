cdb5ae8f5e61e91c1937b264c5ae11041d13e7a2
Jgeissler14 <66270580+Jgeissler14@users.noreply.github.com>
2023-07-24 12:52:32 -0400

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -26,7 +26,7 @@ export default function Hero() {
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h1 className="h1 mb-4" data-aos="fade-up">Need AWS Infrastructure?</h1>
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">
-             Cloudzap scales your AWS with subscriptions to unlimited infrastructure requests delivered in 48 hours on average
+             Cloudzap scales your AWS with subscriptions to unlimited infrastructure delivered in 48 hours on average
               </p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">

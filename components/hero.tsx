1af3638d17c3a734f1c1cad54252b703edb902c9
Joshua Geissler <jrgeissler14@gmail.com>
2023-07-24 10:39:19 -0400

    landing page

--- components/hero.tsx
+++ components/hero.tsx
@@ -24,11 +24,13 @@ export default function Hero() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h1 mb-4" data-aos="fade-up">WANT TO RECOVER 31 ENGINEERING HOURS PER MONTH?</h1>
-            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">31 Hours - that’s the average time your AWS engineers spend in meetings per month. Scale your AWS with subscriptions to unlimited infrastructure delivered asynchronously in 48 hours on average.</p>
+            <h1 className="h1 mb-4" data-aos="fade-up">Need AWS Infrastructure?</h1>
+            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">
+             Cloudzap scales your AWS with subscriptions to unlimited infrastructure requests delivered in 48 hours on average
+              </p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#plans">Reclaim My Time</a>
+                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#plans">Start Requesting</a>
               </div>
               {/* <div data-aos="fade-up" data-aos-delay="600">
                 <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>

6dad132e6aefc804f581bee6a5f574ceaf84ced8
Jgeissler14 <66270580+Jgeissler14@users.noreply.github.com>
2023-10-01 16:55:29 -0400

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -24,7 +24,7 @@ export default function Hero() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h1 mb-4" data-aos="fade-up">Do You Have Inconsistent DevOps Needs?</h1>
+            <h1 className="h1 mb-4" data-aos="fade-up">Do You Have Inconsistent Cloud Needs?</h1>
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">
             Accelerate your startup's growth with reliable, fast, and asynchronous AWS infrastructure. Try us free for 30 days.
             </p>

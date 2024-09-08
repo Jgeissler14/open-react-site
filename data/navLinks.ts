1800d13d28cf78f43e4bb6f7df23a0f22e6ce969
Joshua Geissler <jgeissler14@gmail.com>
2024-09-08 14:13:53 -0700

    headr

--- data/navLinks.ts
+++ data/navLinks.ts
@@ -4,6 +4,14 @@ export const navLinks = [
         href: "/",
     },
     {
+        title: "Book a meeting",
+        href: "https://calendly.com/jrgeissler14/30min",
+    },
+    {
+        title: "YouTube",
+        href: "https://www.youtube.com/@JoshGeissler",
+    },
+    {
         title: "Blog",
         href: "https://blog.cloudzap.co",
     }

628f148fcdb62ec9faa39e1de8f1ad61eb52b2a3
Joshua Geissler <66270580+Jgeissler14@users.noreply.github.com>
2025-06-27 11:22:20 -0700

    new vsl

--- src/App.tsx
+++ src/App.tsx
@@ -134,7 +134,7 @@ function App() {
             <div className="my-8 flex justify-center">
               <div className="w-full h-48 md:w-2/3 md:h-72 lg:h-[480px] flex justify-center">
                 <iframe
-                  src="https://www.youtube.com/embed/K89PvbTobck?si=Lz5Dia9TWy6fl4vX"
+                  src="https://www.youtube.com/embed/EpIFVoWAZ6A?si=q7GfUIx0Ginbfx-e"
                   title="How To Manage Your Cloud Infrastructure With Less Engineers"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
